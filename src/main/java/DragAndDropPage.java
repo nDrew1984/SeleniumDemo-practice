e9a7be6404686f1392235c437aef35a97f2eb319
nDrew <boq93p@gmail.com>
2022-07-26 21:40:04 +0200

    commit20220726

--- src/main/java/DragAndDropPage.java
+++ src/main/java/DragAndDropPage.java
@@ -1,4 +1,11 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class DragAndDropPage extends BasePage {
     public DragAndDropPage(WebDriver driver) {
@@ -6,9 +13,26 @@ public class DragAndDropPage extends BasePage {
     }
 
     private final String url = "https://demo.seleniumeasy.com/drag-and-drop-demo.html";
+    private final By firstElem = By.xpath("//*[@id=\"todrag\"]/span[1]");
+    private final By secondElem = By.xpath("//*[@id=\"todrag\"]/span[2]");
+    private final By thirdElem = By.xpath("//*[@id=\"todrag\"]/span[3]");
+    private final By fourthElem = By.xpath("//*[@id=\"todrag\"]/span[4]");
+    private final By dropZone = By.id("mydropzone");
+    private final By droppedList = By.xpath("//*[@id=\"droppedlist\"]/span");
 
     public void navigate() {
         driver.get(url);
     }
-    // help: https://biercoff.com/moving-slider-and-dragging-elements-with-webdriver/
+    public void dragFirstElem() {
+        Actions actions = new Actions(driver);
+        actions.dragAndDrop(driver.findElement(firstElem), driver.findElement(dropZone)).build().perform();
+    }
+    public List<String> getList() {
+        List<String> result = new ArrayList<>();
+        List<WebElement> list = driver.findElements(droppedList);
+        for (int i = 0; i < list.size(); i++) {
+            result.add(list.get(i).getText());
+        }
+        return result;
+    }
 }
--- src/test/java/TestDragAndDrop.java
+++ src/test/java/TestDragAndDrop.java
@@ -1,4 +1,8 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestDragAndDrop extends BaseTest {
 
@@ -7,6 +11,18 @@ public class TestDragAndDrop extends BaseTest {
         DragAndDropPage dragAndDropPage = new DragAndDropPage(driver);
         dragAndDropPage.navigate();
 
+        dragAndDropPage.dragFirstElem();
+        dragAndDropPage.dragFirstElem();
+        dragAndDropPage.dragFirstElem();
+        dragAndDropPage.dragFirstElem();
 
+        List<String> exp = new ArrayList<>();
+        exp.add("Draggable 1");
+        exp.add("Draggable 2");
+        exp.add("Draggable 3");
+        exp.add("Draggable 4");
+
+        List<String> act = dragAndDropPage.getList();
+        Assertions.assertArrayEquals(exp.toArray(), act.toArray());
     }
 }
