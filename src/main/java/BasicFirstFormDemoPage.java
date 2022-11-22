1e779ef546d01e6c79077704594bb70f45358634
nDrew1984 <boq93p@gmail.com>
2022-11-22 09:49:17 +0100

    commit20221122

--- src/main/java/BasicFirstFormDemoPage.java
+++ src/main/java/BasicFirstFormDemoPage.java
@@ -1,5 +1,11 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
 
 public class BasicFirstFormDemoPage extends BasePage{
     public BasicFirstFormDemoPage(WebDriver driver) {
@@ -11,6 +17,9 @@ public class BasicFirstFormDemoPage extends BasePage{
     private final By valueFieldB = By.id("sum2");
     private final By getTotalButton = By.xpath("//*[@id=\"gettotal\"]/button");
     private final By total = By.id("displayvalue");
+    private final By inputTextField = By.xpath("(//*[@class=\"form-control\"])[1]");
+    private final By showMessageButton = By.xpath("//*[@id=\"get-input\"]/button");
+    private final By textResult = By.id("display");
 
     public void navigate() {
         driver.get(url);
@@ -24,12 +33,53 @@ public class BasicFirstFormDemoPage extends BasePage{
     public void clickGetTotal() {
         driver.findElement(getTotalButton).click();
     }
-    public int totalNumber() {
-        String s = driver.findElement(total).getText();
-        return Integer.parseInt(s);
+    public String totalNumber() {
+        return driver.findElement(total).getText();
+
     }
     public String totalString() {
         return driver.findElement(total).getText();
     }
+    public String getRandomString(int n) {
+        String characters =
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                        + "0123456789"
+                        + "abcdefghijklmnopqrstuvxyz"
+                        + ">#&@{}?:;-_$ß€[]äđ+!=";
+
+        StringBuilder sb = new StringBuilder(n);
+        for (int i = 0; i < n; i++) {
+            int index = (int) (characters.length() * Math.random());
+            sb.append(characters.charAt(index));
+        }
+        return sb.toString();
+    }
+    public void enterText(String s) {
+        driver.findElement(inputTextField).sendKeys(s);
+    }
+    public void clickShowMessageButton() {
+        driver.findElement(showMessageButton).click();
+    }
+    public String getTextResult() {
+        return driver.findElement(textResult).getText();
+    }
+    public int getRandomNumber(int min, int max) {
+        return (int) Math.floor(Math.random()*(max-min+1)+min);
+    }
+    public void clear() {
+        driver.findElement(valueFieldA).clear();
+        driver.findElement(valueFieldB).clear();
+    }
+    public void clearText() {
+        driver.findElement(inputTextField).clear();
+    }
+    public void scrollDown() {
+        JavascriptExecutor scroll = (JavascriptExecutor) driver;
+        scroll.executeScript("window.scrollBy(0,300)", "");
+    }
+
+
+
+
 
 }
--- src/test/java/TestInputFields.java
+++ src/test/java/TestInputFields.java
@@ -1,14 +1,14 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class TestInputFields extends BaseTest{
+public class TestInputFields extends BaseTest {
 
     @Test
     public void TestSumValidNumbers() {
         BasicFirstFormDemoPage basicFirstFormDemoPage = new BasicFirstFormDemoPage(driver);
 
         basicFirstFormDemoPage.navigate();
-        basicFirstFormDemoPage.closePopupWindow();
+        //basicFirstFormDemoPage.closePopupWindow();
 
         // test valid data:
         int a = 5;
@@ -18,9 +18,32 @@ public class TestInputFields extends BaseTest{
         basicFirstFormDemoPage.inputB(String.valueOf(b));
         basicFirstFormDemoPage.clickGetTotal();
 
-        int exp = 11;
-        int act = basicFirstFormDemoPage.totalNumber();
+        String exp = "11";
+        String act = basicFirstFormDemoPage.totalNumber();
         Assertions.assertEquals(exp, act);
+    }
+
+    @Test
+    public void TestSumRandomNumbers() {
+        BasicFirstFormDemoPage basicFirstFormDemoPage = new BasicFirstFormDemoPage(driver);
+        basicFirstFormDemoPage.navigate();
+        basicFirstFormDemoPage.scrollDown();
+
+        for (int i = 0; i < 20; i++) {
+            int a = basicFirstFormDemoPage.getRandomNumber(-1000000000, 1000000000);
+            int b = basicFirstFormDemoPage.getRandomNumber(-1000000000, 1000000000);
+
+            basicFirstFormDemoPage.inputA(String.valueOf(a));
+            basicFirstFormDemoPage.inputB(String.valueOf(b));
+            basicFirstFormDemoPage.clickGetTotal();
+
+            String exp = String.valueOf(a + b);
+            String act = basicFirstFormDemoPage.totalNumber();
+
+            Assertions.assertEquals(exp, act);
+            basicFirstFormDemoPage.clear();
+        }
+        driver.quit();
     }
 
     @Test
@@ -28,7 +51,7 @@ public class TestInputFields extends BaseTest{
         BasicFirstFormDemoPage basicFirstFormDemoPage = new BasicFirstFormDemoPage(driver);
 
         basicFirstFormDemoPage.navigate();
-        basicFirstFormDemoPage.closePopupWindow();
+        //basicFirstFormDemoPage.closePopupWindow();
 
         // test invalid data:
         String a = "abc";
@@ -42,5 +65,40 @@ public class TestInputFields extends BaseTest{
         String act = basicFirstFormDemoPage.totalString();
 
         Assertions.assertEquals(exp, act);
+    }
+
+    @Test
+    public void TestRandomMessages() {
+        BasicFirstFormDemoPage basicFirstFormDemoPage = new BasicFirstFormDemoPage(driver);
+        basicFirstFormDemoPage.navigate();
+
+        for (int i = 0; i < 20; i++) {
+            String exp = basicFirstFormDemoPage.getRandomString(10);
+            basicFirstFormDemoPage.enterText(exp);
+
+            basicFirstFormDemoPage.clickShowMessageButton();
+
+            String act = basicFirstFormDemoPage.getTextResult();
+            Assertions.assertEquals(exp, act);
+
+            basicFirstFormDemoPage.clearText();
+        }
+        driver.quit();
+    }
+
+    @Test
+    public void TestMessage() {
+        BasicFirstFormDemoPage basicFirstFormDemoPage = new BasicFirstFormDemoPage(driver);
+        basicFirstFormDemoPage.navigate();
+
+        String exp = "<>#&@{}?:;-_$ß€[]äđ+!=";
+        basicFirstFormDemoPage.enterText(exp);
+
+        basicFirstFormDemoPage.clickShowMessageButton();
+
+        String act = basicFirstFormDemoPage.getTextResult();
+        Assertions.assertEquals(exp, act);
+
+        driver.quit();
     }
 }
